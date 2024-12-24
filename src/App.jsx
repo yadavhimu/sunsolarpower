4adf999fadd10c74e9e09b29a295a52c22729d19
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-12-24 16:31:23 +0530

    pages updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Products from './pages/Products'
-import Resources from './pages/Resources'
+// import Resources from './pages/Resources'
 import About from './pages/About'
 import Contact from './pages/Contact'
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
        <Route path='/' element={<Home/>}/>
        <Route path='/products' element={<Products/>}/>
-       <Route path='/resources' element={<Resources/>}/>
+       {/* <Route path='/resources' element={<Resources/>}/> */}
        <Route path='/about' element={<About/>}/>
        <Route path='/contact' element={<Contact/>}/>
       </Routes>
